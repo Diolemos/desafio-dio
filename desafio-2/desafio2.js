ee2c91a9440f7bbffba39e0ab877cc4a9f3a85d1
Dio Lemos <67412764+Diolemos@users.noreply.github.com>
2025-07-08 22:48:41 -0300

    update desafio2.js

--- desafio-2/desafio2.js
+++ desafio-2/desafio2.js
@@ -1,3 +1,65 @@
+class Hero {
+    //campos privados:
+    #name;
+    #vitorias;
+    #derrotas;
+
+    constructor(name, xp, health) {
+        this.#name = name;
+        this.xp = xp;
+        this.health = health;
+        this.#vitorias = 10000;
+        this.#derrotas = 1;
+    }
+
+    gainXp(amount) {
+        this.xp += amount;
+    }
+
+    takeDamage(amount) {
+        this.health -= amount;
+    }
+
+    heal(amount) {
+        this.health += amount;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    get vitorias() {
+        return this.#vitorias;
+    }
+
+    get derrotas() {
+        return this.#derrotas;
+    }
+
+    get level() {
+        if (this.xp < 1000) return "Iron";
+        if (this.xp < 2000) return "Bronze";
+        if (this.xp < 5000) return "Silver";
+        if (this.xp < 8000) return "Gold";
+        if (this.xp < 9000) return "Diamond";
+        if (this.xp < 10000) return "Rising";
+        if (this.xp <= 10000) return "Immortal";
+        return "Effulgent";
+    }
+
+    status() {
+        return `${this.name} | XP: ${this.xp} | Health: ${this.health} | Level: ${this.level}`;
+    }
+}
+
+
+const beowulf = new Hero("Beowulf", 0, 100);
+
+beowulf.gainXp(5100);
+beowulf.takeDamage(50);
+beowulf.heal(25);
+//console.log(beowulf.status());
+
 
 
 function nivelarJogador(saldoVitorias) {
@@ -24,10 +86,10 @@ function nivelarJogador(saldoVitorias) {
 
 
 
-const gerarSaldoVitorias=(quantidadeVitorias, quantidadeDerrotas) => quantidadeVitorias - quantidadeDerrotas;
+const gerarSaldoVitorias = (quantidadeVitorias, quantidadeDerrotas) => quantidadeVitorias - quantidadeDerrotas;
 
-let saldoVitorias = gerarSaldoVitorias(1000,33);
+let saldoVitorias = gerarSaldoVitorias(beowulf.vitorias, beowulf.derrotas);
 
 let nivel = nivelarJogador(saldoVitorias);
 
-console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`) ;
+console.log(`O Herói ${beowulf.name} tem de saldo de ${saldoVitorias} está no nível de ${nivel}`);
